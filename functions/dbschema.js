abdce1ecafe91f902f74331deacb2018faaaa8bf
clayhindman01 <clayhindman01@gmail.com>
2021-08-02 20:58:17 -0400

    test

---
+++ functions/dbschema.js
@@ -0,0 +1,12 @@
+let db = {
+    dribbles: [
+        {
+            userHandle: 'user',
+            body: 'this is the dribble body',
+            createdAt: '2021-08-02T15:56:15.429Z',
+            likeCount: 5,
+            commentCount: 2
+
+        }
+    ]
+}
